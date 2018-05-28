837c0a72c46d7780c8941f2c94f31796ee74ee1c
korntip <korntip@skyict.co.th>
2018-05-28 11:23:22 +0700

    Create MainFragment Success

--- app/src/main/java/korntip/skyict/co/th/ticketservice/MainActivity.java
+++ app/src/main/java/korntip/skyict/co/th/ticketservice/MainActivity.java
@@ -3,11 +3,23 @@ package korntip.skyict.co.th.ticketservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import korntip.skyict.co.th.ticketservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFrgment, new MainFragment())
+                    .commit();
+
+        }
+    }//Main Method
+}//Class
